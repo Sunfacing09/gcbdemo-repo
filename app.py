1d081a743c7082ed9248341d4a4b4013f522536b
KO WEI CHEN <waynechen@Waynes-MacBook-Pro.local>
2022-02-11 18:47:03 +0800

    Add: promotion page

--- app.py
+++ app.py
@@ -9,5 +9,14 @@ def hello():
     html = "<h3>Hello, World! It worked Again</h3>"
     return html
 
+
+@app.route("/promotion")
+def promotion():
+    html = "<h3>It's promotion page</h3>"
+    return html
+
+
+
+
 if __name__ == "__main__":
   app.run(host='0.0.0.0', port=8080)
