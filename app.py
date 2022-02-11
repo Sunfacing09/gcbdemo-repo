824ac3cfb11fb3ea8f8ab97b9602c4977979e565
KO WEI CHEN <waynechen@Waynes-MacBook-Pro.local>
2022-02-11 23:56:08 +0800

    Add:Index&Promotion Pages

--- app.py
+++ app.py
@@ -1,22 +1,23 @@
-from flask import Flask
+from flask import Flask, render_template, request
 import os
 import socket
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=['GET'])
 def hello():
     html = "<h3>Hello, World! It worked Again</h3>"
-    return html
+    return render_template('index.html')
 
 
-@app.route("/promotion")
-def promotion():
+@app.route("/<promotion>", methods=['GET'])
+def promotion(promotion="Nothing"):
+    promotion = request.args.get('promotion', promotion)
     html = "<h3>It's promotion page</h3>"
-    return html
+    return promotion
 
 
 
 
 if __name__ == "__main__":
-  app.run(host='0.0.0.0', port=8080)
+  app.run(debug=True, host='0.0.0.0', port=8080)
